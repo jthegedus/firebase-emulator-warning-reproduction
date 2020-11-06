eec4ebd02baefabf0f53b9aebe63557e69aa03e1
jthegedus <jthegedus@hey.com>
2020-11-06 18:38:28 +1100

    tests: remove gcloud cli

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "hello tests"
-gcloud pubsub topics publish topic-name --message 'Ahoy hoy from gcloud'
 curl -X POST -H "Content-Type:application/json" -H "X-MyHeader: 123" http://localhost:5001/jthegedus-firebase-extensions/us-central1/helloWorld --data '{"text":"ahoy hoy from http via curl"}'
 curl -X POST -H 'content-type:application/json' -H "X-MyHeader: 123" http://localhost:8085/v1/projects/jthegedus-firebase-extensions/topics/topic-name:publish --data $'{ "messages": [{"data": "YWhveSBob3kgZnJvbSBwdWJzdWIgdmlhIGN1cmw="}]}'
